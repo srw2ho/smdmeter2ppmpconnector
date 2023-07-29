67e6f5e10e3feb102c0937036e5e574236732510
balciselman <selman.balci@gmail.com>
2023-07-29 12:52:58 +0200

    changes

--- builddocker.sh
+++ builddocker.sh
@@ -28,10 +28,11 @@ echo -e " build dockerimage: $dockerimage  Build Version:" $Build
 
 
 # sudo docker build --build-arg NODE_ENV=development --build-arg BUILDCMD=dev -f Dockerfile.MH -t grafana/grafana-mhdev:v7.6.5 .
-sudo docker build -t ${dockerimage}:${Build}  -f Dockerfile .
+sudo docker build --no-cache -t ${dockerimage}:${Build}  -f Dockerfile .
 
 # @docker build . -t %dockerimage%:%Build%
 
 # save images in ../docker_images
+
 mkdir ../docker_images
 docker save -o ../docker_images/${dockerimage}_${Build}.zip ${dockerimage}:${Build}
