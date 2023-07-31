87d59117a1aac51ef3ddb7dfcd81d5a3d2a1c998
balciselman <selman.balci@gmail.com>
2023-07-31 20:27:26 +0200

    bugfixes

--- growatt/growatt_SPH_TL3_BH_UP.py
+++ growatt/growatt_SPH_TL3_BH_UP.py
@@ -60,7 +60,7 @@ class SPH_TL3_BH_UP(Growatt):
             "Epv2_today": (63, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "PV2_Energy today", "kW", 2, 0.1),
             "Epv2_total": (65, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "PV2_Energy total", "kW", 2, 0.1),
 
-
+            "Epv_total": (91, 2, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "PV Energy total", "kW", 3, 0.1),
             "Temp1": (93, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Inverter temperature", "°C", 3, 0.1),
             "Temp2": (94, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "inside IPM in inverter Temperature", "°C", 3, 0.1),
             "Temp3": (95, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Boost temperature", "°C", 3, 0.1),
@@ -75,13 +75,13 @@ class SPH_TL3_BH_UP(Growatt):
             "Pactouser_T": (1019, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "AC power to user total", "W", 4, 0.1),
             "Pactouser_Total": (1021, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "AC power to user total", "W", 4, 0.1),
             "Pac_to_grid_R": (1023, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "AC power to grid", "W", 4, 0.1),
-            
+           
             "uwSysWorkMode": (1000, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "System work mode", "", 5, 1),
-            
-            "LocalLoad_total": (1037, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "INV_power to local load total", "W", 6, 0.1),
-            "Etouser_today": (1044, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to user today", "W", 6, 0.1),
-            "Etouser_total": (1046, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to user total", "W", 6, 0.1),
-            "Etogrid_today": (1048, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to grid today", "W", 6, 0.1),
+            "PLocalLoad_total": (1037, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "INV_power to local load total", "W", 6, 0.1),
+         
+            "Etouser_today": (1044, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to user today", "KWh", 6, 0.1),
+            "Etouser_total": (1046, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to user total", "KWh", 6, 0.1),
+            "Etogrid_today": (1048, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to grid today", "KWh", 6, 0.1),
            
             "Etogrid_total": (1050, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Energy_to grid total", "W", 6, 0.1),
             "Edischarge1_today": (1052, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Discharge_energy1 today", "W", 6, 0.1),
@@ -92,12 +92,14 @@ class SPH_TL3_BH_UP(Growatt):
             "ELocalLoad_Total": (1062, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "Local_load energy total", "W", 6, 0.1),
             "dwExportLimitApparentPower": (1064, 4, meter.registerType.INPUT, meter.registerDataType.UINT32, float, "ExportLimitApparentPower", "W", 6, 0.1),
             
+            "BMS_StatusOld": (1082, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, int, "old Status from BMS", "", 7, 1),
             "BMS_Status": (1083, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, int, "Status from BMS", "", 7, 1),
+            "BMS_ErrorOld": (1084, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, int, "Old Error infomation from BMS", "", 7, 1),
             "BMS_Error": (1085, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, int, "Error infomation from BMS", "", 7, 1),
             "BMS_SOC": (1086, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "SOC from BM", "%", 7, 1),
-            "BMS_BatteryVolt": (1087, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery voltage from BMS", "V", 7, 1),
-            "BMS_BatteryCurrent": (1088, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery current from BMS", "A", 7, 1),
-            "BMS_BatteryTemp": (1089, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery temperature from BMS", "°C", 7, 1),
+            "BMS_BatteryVolt": (1087, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery voltage from BMS", "V", 7, 0.1),
+            "BMS_BatteryCurrent": (1088, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery current from BMS", "A", 7, 0.001),
+            "BMS_BatteryTemp": (1089, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Battery temperature from BMS", "°C", 7, 0.1),
             
             "BMS_SOH": (1096, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "BMS_SOH", "", 8, 1),
             "uwMaxCellVolt": (1108, 2, meter.registerType.INPUT, meter.registerDataType.UINT16, float, "Maximum single battery voltage", "V", 9, 0.001),
@@ -114,7 +116,7 @@ class SPH_TL3_BH_UP(Growatt):
 
             "Reactive_P_Rate": (4, 2, meter.registerType.HOLDING, meter.registerDataType.UINT16, int, "Inverter max output reactive power percent", "%", 1, 0.1),
 
-            "Power_factor": (5, 2, meter.registerType.HOLDING, meter.registerDataType.UINT16, int, "Inverter output power factor’s 10000 times", "%", 1, 1000),
+            "Power_factor": (5, 2, meter.registerType.HOLDING, meter.registerDataType.UINT16, int, "Inverter output power factor’s 10000 times", "%", 1, 1),
 
             "ExportLimit_EN_Dis": (122, 2, meter.registerType.HOLDING, meter.registerDataType.UINT16, int, "ExportLimit_En/dis", "0...3", 2, 1),
             "ExportLimitPowerRate": (123, 2, meter.registerType.HOLDING, meter.registerDataType.UINT16, int, "ExportLimitPowerRate", "%", 2, 0.1),
